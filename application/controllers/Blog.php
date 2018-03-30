7b4764bb0a7788109551d7274259e907c86b6005
Anita Lad <jagtapanita@gmail.com>
2018-03-30 08:39:06 +0530

    Added blog controller and view

---
+++ application/controllers/Blog.php
@@ -0,0 +1,14 @@
+<?php
+
+class Blog extends CI_Controller {
+
+	public function index(){
+		//echo "In index blog controller";
+		$this->load->view('blog_index');
+	}
+
+	public function add(){
+		echo "Add blog in a controller";
+	} 
+}
+?>
---
+++ application/views/blog_index.php
@@ -0,0 +1,7 @@
+<!doctype>
+<html>
+	<title>This is the blog index</title>	
+	<body>
+		<h1>This is the blog index</h1>
+	</body>
+</html>
